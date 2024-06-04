41dc761d577af1f51b2571a685c99ce242052320
thanigai-babu17 <thanigai@coderapper.com>
2024-06-04 18:25:34 +0530

    ignored firebase key from git

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,6 @@ if (host === 'localhost') {
 }
 
 export default defineConfig({
-  optimizeDeps: { exclude: ['fsevents'] },
   server: {
     port: Number(process.env.PORT || 3000),
     hmr: hmrConfig,
